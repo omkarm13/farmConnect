2b70bad23ee5405ae26fb6ac8de35f70a9288e47
shrivardhan mohite <shrivardhan18188@gmail.com>
2025-02-17 19:13:38 +0530

    sorting added

--- routes/vegetable.js
+++ routes/vegetable.js
@@ -1,34 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-const {isOwner, isFarmer} = require("../middleware.js");
+const { isOwner, isFarmer, isAuth } = require("../middleware.js");
 const vegetableController = require("../controllers/vegetables.js");
-const { isAuth } = require("../middleware.js");
-const TryCatch = require("../utils/TryCatch.js");
-const multer  = require('multer');
-const {storage} = require("../cloudConfig.js");
-const upload = multer({ storage})
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
-router.route("/")
-.get(wrapAsync(vegetableController.indexVegetable))
-.post(isAuth, isFarmer, upload.single('image'), wrapAsync(vegetableController.createVegetable));
-// .post(upload.single('image'), (req, res, next) =>{
-    
-// });
+router
+  .route("/")
+  .get(wrapAsync(vegetableController.indexVegetable)) // Sorted in the controller
+  .post(isAuth, isFarmer, upload.single("image"), wrapAsync(vegetableController.createVegetable));
 
-router.get("/", wrapAsync(vegetableController.indexVegetable));
-
-//New Route
 router.get("/new", isAuth, isFarmer, vegetableController.newVegetable);
 
-router.route("/:id")
-.get(wrapAsync(vegetableController.showVegetable))
-.put(isAuth, isFarmer, isOwner, wrapAsync(vegetableController.updateVegetable))
-// .delete(isAuth, isFarmer, isOwner, wrapAsync(vegetableController.deleteVegetable));
+router
+  .route("/:id")
+  .get(wrapAsync(vegetableController.showVegetable))
+  .put(isAuth, isFarmer, isOwner, wrapAsync(vegetableController.updateVegetable));
 
-// //Edit Route
 router.get("/:id/edit", isAuth, isFarmer, isOwner, wrapAsync(vegetableController.editVegetable));
-
-
 
 module.exports = router;
